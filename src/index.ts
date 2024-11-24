3cd317a8019f1e3edfd71a8f6edf882810ac45ea
RustiktamQ <rramilperm@gmail.com>
2024-11-24 17:58:08 +0500

    add runes notif

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,38 @@
 // import "./translations"
 
 import {
+	Entity,
 	EventsSDK,
-	GameRules,
-	GameState,
 	NotificationsSDK,
-	PlayerCustomData,
-	Rectangle,
+	Rune
 } from "github.com/octarine-public/wrapper/index"
 
-new (class CNotification {
-	constructor() {
+import { MenuManager } from "./menu"
+import { GameNotification } from "./notification"
 
+new (class CNotifications {
+	private readonly menu = new MenuManager()
+
+	constructor() {
+		EventsSDK.on("EntityCreated", this.EntityCreated.bind(this))
+	}
+
+	protected EntityCreated(entity: Entity) {
+		if (!this.menu.State.value) {
+			return
+		}
+		if (!(entity instanceof Rune) || this.menu.runeState.value) {
+			console.log(0)
+			return
+		}
+
+		console.log(1)
+
+		NotificationsSDK.Push(
+			new GameNotification(
+				"soundboard.ay_ay_ay_cn",
+				"panorama/images/hud/reborn/rune_doubledamage.vtex_c"
+			)
+		)
 	}
 })()
