40e91de27d87d75fe66b399a720d5de106753701
RustiktamQ <rramilperm@gmail.com>
2024-11-25 20:57:23 +0500

    add rune remind notif

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import {
-	Entity,
+	DOTAGameMode,
 	EventsSDK,
 	GameRules,
 	ImageData,
-	NotificationsSDK,
-	Rune
+	NotificationsSDK
 } from "github.com/octarine-public/wrapper/index"
 
 import { MenuManager } from "./menu"
@@ -12,37 +11,88 @@ import { GameNotification } from "./notification"
 
 new (class CNotifications {
 	private readonly menu = new MenuManager()
+	private cooldowns: Record<"active" | "bounty" | "xp", number> = {
+		active: 0,
+		bounty: 0,
+		xp: 0
+	}
+
+	private readonly spawnTimes: Record<"active" | "bounty" | "xp", number> = {
+		active: 120,
+		bounty: 180,
+		xp: 420
+	}
 
 	constructor() {
-		EventsSDK.on("EntityCreated", this.EntityCreated.bind(this))
 		EventsSDK.on("PostDataUpdate", this.OnTick.bind(this))
 	}
 
 	protected OnTick(dt: number) {
-		if (dt === 0 || !this.menu.runeRemindState.value) {
+		if (dt === 0 || !this.menu.runeRemindState.value || !GameRules) {
 			return
 		}
 
-		console.log(this.timeToSpawnRune)
-	}
+		const currentTime = GameRules.GameTime
+		Object.keys(this.cooldowns).forEach(key => {
+			const type = key as "active" | "bounty" | "xp"
+			if (this.cooldowns[type] !== 0 && currentTime - this.cooldowns[type] > 2) {
+				this.cooldowns[type] = 0
+			}
+		})
 
-	protected EntityCreated(entity: Entity) {
-		if (!this.menu.State.value) {
-			return
+		const textureMap: Record<"active" | "bounty" | "xp", string> = {
+			active: "regen",
+			bounty: "bounty",
+			xp: "xp"
 		}
-		if (entity instanceof Rune && this.menu.runeState.value) {
-			NotificationsSDK.Push(
-				new GameNotification(
+
+		for (const type of ["active", "bounty", "xp"] as const) {
+			const remainingTime = this.getRemainingTime(this.getSpawnTime(type))
+			const texture = textureMap[type]
+			if (remainingTime > 0 && remainingTime < 0.1) {
+				this.SendNotif(
 					"soundboard.ay_ay_ay_cn",
-					ImageData.GetRuneTexture(entity.Name),
-					"Spawned"
+					ImageData.GetRuneTexture(texture),
+					"spawned",
+					type
 				)
-			)
+			} else if (remainingTime > 20 && remainingTime < 20.1) {
+				this.SendNotif(
+					"soundboard.ay_ay_ay_cn",
+					ImageData.GetRuneTexture(texture),
+					"20 second!",
+					type
+				)
+			}
 		}
 	}
 
-	private get timeToSpawnRune() {
-		// TO DO
-		return GameRules?.GameTime
+	protected SendNotif(
+		sound: string,
+		image: string,
+		text: string,
+		type: "active" | "bounty" | "xp"
+	) {
+		if (this.cooldowns[type] !== 0) {
+			return
+		}
+		NotificationsSDK.Push(new GameNotification(sound, image, text))
+		this.cooldowns[type] = GameRules?.GameTime ?? 0
+	}
+
+	// TODO: disable in custom games & rework in other games modes (ex. solo mid 1 vs 1, etc.)
+	private getSpawnTime(type: "active" | "bounty" | "xp"): number {
+		const spawn = this.spawnTimes[type]
+		return GameRules?.GameMode === DOTAGameMode.DOTA_GAMEMODE_TURBO
+			? spawn / 2
+			: spawn
+	}
+
+	private getModuleTime(spawnTime: number): number {
+		return (GameRules?.GameTime ?? 0) % Math.floor(spawnTime)
+	}
+
+	private getRemainingTime(spawnTime: number): number {
+		return Math.max(0, spawnTime - this.getModuleTime(spawnTime))
 	}
 })()
